241c0d122775d797664325388c5b2e3dce8a47d5
Kristoffer Nowén <kristoffer.nowen@learnet.se>
2024-05-08 11:23:21 +0200

    add cors policy

--- Learner.Api/Program.cs
+++ Learner.Api/Program.cs
@@ -1,5 +1,6 @@
 using Learner.Application;
 using Learner.Persistence;
+using Microsoft.Net.Http.Headers;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,18 +14,30 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddApplicationExtensions();
 builder.Services.AddPersistenceExtensions(builder.Configuration);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("learner", policyBuilder =>
+        {
+            policyBuilder.WithOrigins($"{builder.Configuration.GetValue<string>("allowOrigin")}");
+            policyBuilder.WithMethods("GET", "DELETE", "POST", "UPDATE");
+            policyBuilder.WithHeaders(HeaderNames.ContentType);
+        }
+    );
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseSwagger();
+    //place app.UseSwagger outside if block, if you want to use azure api management to work according to https://learn.microsoft.com/en-us/aspnet/core/tutorials/publish-to-azure-api-management-using-vs?view=aspnetcore-8.0 It needs to read the json documentation by Swagger to configure the layer between
 }
 
 app.UseHttpsRedirection();
 
-app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+app.UseCors("learner");
 
 app.UseAuthorization();
 
