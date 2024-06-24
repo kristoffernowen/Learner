623584bb563357ff56fca8161081af1322cc35aa
Kristoffer Nowén <kristoffer.nowen@learnet.se>
2024-06-24 13:19:52 +0200

    add logic to handler, but not implemented yet

--- Learner.Application.Tests/ExercisesTests/SingleFactExerciseTests/CreateSingleFactExerciseHandler.cs
+++ Learner.Application.Tests/ExercisesTests/SingleFactExerciseTests/CreateSingleFactExerciseHandler.cs
@@ -1,11 +1,26 @@
-﻿using MediatR;
+﻿using AutoMapper;
+using MediatR;
 
 namespace Learner.Application.Tests.ExercisesTests.SingleFactExerciseTests;
 
 public class CreateSingleFactExerciseHandler : IRequestHandler<CreateSingleFactExerciseCommand, CreateSingleFactExerciseOutputDto>
 {
-    public Task<CreateSingleFactExerciseOutputDto> Handle(CreateSingleFactExerciseCommand request, CancellationToken cancellationToken)
+    private readonly ISingleFactExerciseRepository _singleFactExerciseRepository;
+    private readonly IMapper _mapper;
+
+    public CreateSingleFactExerciseHandler(ISingleFactExerciseRepository singleFactExerciseRepository, IMapper mapper)
     {
-        throw new NotImplementedException();
+        _singleFactExerciseRepository = singleFactExerciseRepository;
+        _mapper = mapper;
+    }
+    public async Task<CreateSingleFactExerciseOutputDto> Handle(CreateSingleFactExerciseCommand request, CancellationToken cancellationToken)
+    {
+        
+
+        var exercise = SingleFact.Create(request);
+
+        var persistedExercise = await _singleFactExerciseRepository.Create(exercise);
+
+        return _mapper.Map<CreateSingleFactExerciseOutputDto>(persistedExercise);
     }
 }
