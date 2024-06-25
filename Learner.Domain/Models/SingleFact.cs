ccd23d10e688882e4cdfc9bc025c5595d961282d
Kristoffer Nowén <kristoffer.nowen@learnet.se>
2024-06-25 10:54:20 +0200

    put single fact in seperate file

---
+++ Learner.Domain/Models/SingleFact.cs
@@ -0,0 +1,7 @@
+﻿namespace Learner.Domain.Models;
+
+public class SingleFact : BaseFact
+{
+    public string SingleFactExerciseId { get; set; } = null!;
+    public SingleFactExercise SingleFactExercise { get; set; } = null!;
+}
--- Learner.Domain/Models/SingleFactExercise.cs
+++ Learner.Domain/Models/SingleFactExercise.cs
@@ -6,10 +6,4 @@
         public List<SingleFact> Facts { get; set; } = [];
         
     }
-
-    public class SingleFact : BaseFact
-    {
-        public string SingleFactExerciseId { get; set; } = null!;
-        public SingleFactExercise SingleFactExercise { get; set; } = null!;
-    }
 }
