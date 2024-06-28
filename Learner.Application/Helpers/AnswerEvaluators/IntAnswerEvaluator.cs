5f3730e01e8f66622cb9ba904208333dbeaa02ca
Kristoffer Nowén <kristoffer.nowen@learnet.se>
2024-06-28 22:23:46 +0200

    add override for checkanswer inttype answerevaluator, whitespaces should not matter

--- Learner.Application/Helpers/AnswerEvaluators/IntAnswerEvaluator.cs
+++ Learner.Application/Helpers/AnswerEvaluators/IntAnswerEvaluator.cs
@@ -1,7 +1,19 @@
-﻿namespace Learner.Application.Helpers.AnswerEvaluators;
+﻿using Learner.Application.Helpers.ConversionHelpers;
+using Learner.Domain.Models;
+
+namespace Learner.Application.Helpers.AnswerEvaluators;
 
 public class IntAnswerEvaluator : AnswerEvaluator
 {
+    public override bool CheckIfAnswersAreEqual(string givenAnswer, BaseFact fact)
+    {
+        var givenInt = FactConversion.GetIntFromFact(givenAnswer);
+        var correctInt = FactConversion.GetIntFromFact(fact.FactValue);
+        var givenMeasure = FactConversion.GetMeasureFromFact(givenAnswer);
+        var correctMeasure = FactConversion.GetMeasureFromFact(fact.FactValue);
+        return givenInt == correctInt && givenMeasure == correctMeasure;
+    }
+
     public bool IsGreaterThanAnswer()
     {
         throw new NotImplementedException();
